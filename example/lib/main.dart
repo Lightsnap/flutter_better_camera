ee511782d60d7fd7bbf662155b97518b4298fb8f
Guðmundur Egill <geb.2991@gmail.com>
2020-07-03 16:57:13 +0000

    Change the name of the project to flutter_better_camera and add homePage

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:camera/camera.dart';
+import 'package:flutter_better_camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
